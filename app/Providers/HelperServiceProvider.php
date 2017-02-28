55371a4be5621459ea727431c0e96393b9339085
rxluz <rx@appock.co>
2017-02-28 00:17:18 -0300

    helpers test

---
+++ app/Providers/HelperServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      foreach (glob(app_path().'/Helpers/*.php') as $filename){
+          require_once($filename);
+      }
+    }
+}

35cafcbd11230cedb6e3c760e06aff9dcad68dce
rxluz <rx@appock.co>
2017-02-28 00:45:05 -0300

    changes 16

--- app/Providers/HelperServiceProvider.php
+++ app/Providers/HelperServiceProvider.php
@@ -30,6 +30,8 @@ class HelperServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+      foreach (glob(app_path().'/Helpers/*.php') as $filename){
+          require_once($filename);
+      }
     }
 }

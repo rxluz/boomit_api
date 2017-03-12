d6f6f72294b5550d8987067104534085a647aa5c
rxluz <rx@appock.co>
2017-03-11 23:02:35 -0300

    shell

--- routes/api/app/others.php
+++ routes/api/app/others.php
@@ -16,19 +16,24 @@ $response["200"]=function(){
 */
 
 Route::post('contacts', 'App\OthersController@storeContacts');
-Route::post('test/{testparam?}', function(){
-  echo
-    set__rule('data')
-    ->type('exists')
-    ->table("users")
-    ->fields(
-      [
-        ["name" => "id", "value" => is__greater(get__post("min_id"))]
-      ]
-    )
-    ->excepts(
-      [
-        ["name" => "active", "value" => is__value("1")]
-      ]
-    );
+
+Route::post('shell_quiz', function(){
+  return response('olar mundo', 200);
 });
+
+// Route::post('test/{testparam?}', function(){
+//   echo
+//     set__rule('data')
+//     ->type('exists')
+//     ->table("users")
+//     ->fields(
+//       [
+//         ["name" => "id", "value" => is__greater(get__post("min_id"))]
+//       ]
+//     )
+//     ->excepts(
+//       [
+//         ["name" => "active", "value" => is__value("1")]
+//       ]
+//     );
+// });

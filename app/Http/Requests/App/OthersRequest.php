fc4435cf55adaba85e1eae859dccd210d93283db
rxluz <rx@appock.co>
2017-03-11 23:08:54 -0300

    shell 2

--- app/Http/Requests/App/OthersRequest.php
+++ app/Http/Requests/App/OthersRequest.php
@@ -40,10 +40,30 @@ class OthersRequest extends ValidateRequest
     ];
 
 
+    protected $updateShellQuiz=[
+      'e1_nome_completo' => [
+        'required'
+      ]
+      , 'e1_email' => [
+          'required'
+        , 'email'
+      ]
+      , 'e1_data_nascimento' => [
+        'required'
+      ]
+      , 'e1_seu_proposito' => [
+        'max:400'
+      ]
+      , 'e1_grau_escolaridade' => []
+      , 'e1_hoje_esta' => []
+    ];
+
     public function customRules()
     {
         return [
-          "storeContacts" => $this->storeContacts
+            "storeContacts" => $this->storeContacts
+          , "updateShellQuiz" => $this->updateShellQuiz
+          , "storeShellQuiz" => $this->storeShellQuiz
         ];
     }
 

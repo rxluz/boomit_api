82183ab4c20f89bdece162744cb82a2e64513dbe
rxluz <rx@appock.co>
2017-03-13 00:30:17 -0300

    mail send 30

--- resources/views/shell.blade.php
+++ resources/views/shell.blade.php
@@ -413,7 +413,7 @@ Sizes: [
 																			<tr>
 																				<td valign="top" class="textContent">
 																					<h3 style="color:#5F5F5F;line-height:125%;font-family:Helvetica,Arial,sans-serif;font-size:20px;font-weight:normal;margin-top:0;margin-bottom:3px;text-align:left;">Nome completo</h3>
-																					<div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:3px;color:#5F5F5F;line-height:135%;">{{ $e1_nome_completo }}.</div>
+																					<div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:3px;color:#5F5F5F;line-height:135%;">{{ $shell_quiz->e1_nome_completo }}.</div>
 																				</td>
 																			</tr>
 																		</table>

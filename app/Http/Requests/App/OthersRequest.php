caaea0d295d60634c737a06076009678eea1879f
rxluz <rx@appock.co>
2017-03-12 21:18:50 -0300

    test

--- app/Http/Requests/App/OthersRequest.php
+++ app/Http/Requests/App/OthersRequest.php
@@ -20,40 +20,97 @@ class OthersRequest extends ValidateRequest
     ];
 
     protected $storeShellQuiz=[
-      'e1_nome_completo' => [
-        'required'
-      ]
-      , 'e1_email' => [
-          'required'
-        , 'email'
-      ]
-      , 'e1_data_nascimento' => [
-        'required'
-      ]
-      , 'e1_seu_proposito' => [
-        'max:400'
-      ]
-      , 'e1_grau_escolaridade' => []
-      , 'e1_hoje_esta' => []
+        'e1_nome_completo' => ['required'] //ok
+      , 'e1_email' => ['required', 'email'] //ok
+      , 'e1_data_nascimento' => ['required'] //ok
+      , 'e1_seu_proposito' => ['max:400'] //ok
+      , 'e1_grau_escolaridade' => [] //ok
+
+      , 'e1_hoje_esta' => [] //ok
+      , 'e1_mora_com' => ['json'] //ok
+      , 'e1_sobre_renda' => [] //ok
+      , 'e1_estagio_negocio' => [] //ok
+      , 'e1_tempo_dia' => []  //ok
+      , 'e1_avalia_resultados' => [] //ok
+      , 'e1_apoio_familiar' => [] //ok
+      , 'e1_ideal_negocio' => [] //ok
+      , 'e1_fracassou_antes' => [] //ok
+      , 'e1_tempo_objetivo' => [] //ok
+      , 'e1_proposito_detalhado' => ['max:1500'] //ok
+      , 'e2_competencias' => ['json'] //ok
+      , 'e2_fragilidades' => ['json'] //ok
+      , 'e2_melhor_forca' => [] //ok
+      , 'e2_pior_fragilidade' => [] //ok
+
+      , 'e2_forca_parceiros' => []  //ok
+      , 'e2_corresponde_forca' => [] //ok
+      , 'e2_mantem_forca' => [] //?
+      , 'e2_forcas_final' => ['json'] //ok
+
+      , 'e2_fragilidade_parceiros' => ['json'] //
+      , 'e2_corresponde_fragilidade' => ['json']
+      , 'e2_mantem_fragilidade' => []
+      , 'e2_fragilidades_final' => ['json']
+
+
+      , 'e2_forca_fragilidade' => [] //ok
+      , 'e2_justificativa_forca_fragilidade' => ['max:900'] //ok
+      , 'e2_forca_1' => ['max:400'] //ok
+      , 'e2_forca_2' => ['max:400'] //ok
+      , 'e2_forca_3' => ['max:400'] //ok
+      , 'e2_fragilidade_1' => ['max:400'] //ok
+      , 'e2_fragilidade_2' => ['max:400'] //ok
+      , 'e2_fragilidade_3' => ['max:400'] //ok1
+
+      , 'e3_como_entrei' => ['max:1200'] //ok
+      , 'e3_como_saio' => ['max:1200'] //ok
     ];
 
 
     protected $updateShellQuiz=[
-      'e1_nome_completo' => [
-        'required'
-      ]
-      , 'e1_email' => [
-          'required'
-        , 'email'
-      ]
-      , 'e1_data_nascimento' => [
-        'required'
-      ]
-      , 'e1_seu_proposito' => [
-        'max:400'
-      ]
-      , 'e1_grau_escolaridade' => []
-      , 'e1_hoje_esta' => []
+        'e1_nome_completo' => [] //ok
+      , 'e1_email' => ['email'] //ok
+      , 'e1_data_nascimento' => [] //ok
+      , 'e1_seu_proposito' => ['max:400'] //ok
+      , 'e1_grau_escolaridade' => [] //ok
+      , 'e1_hoje_esta' => [] //ok
+      , 'e1_mora_com' => ['json'] //ok
+      , 'e1_sobre_renda' => [] //ok
+      , 'e1_estagio_negocio' => [] //ok
+      , 'e1_tempo_dia' => []  //ok
+      , 'e1_avalia_resultados' => [] //ok
+      , 'e1_apoio_familiar' => [] //ok
+      , 'e1_ideal_negocio' => [] //ok
+      , 'e1_fracassou_antes' => [] //ok
+      , 'e1_tempo_objetivo' => [] //ok
+      , 'e1_proposito_detalhado' => ['max:1500'] //ok
+      , 'e2_competencias' => ['json'] //ok
+      , 'e2_fragilidades' => ['json'] //ok
+      , 'e2_melhor_forca' => [] //ok
+      , 'e2_pior_fragilidade' => [] //ok
+
+      , 'e2_forca_parceiros' => []  //ok
+      , 'e2_corresponde_forca' => [] //ok
+      , 'e2_mantem_forca' => [] //?
+      , 'e2_forcas_final' => ['json'] //ok
+
+      , 'e2_fragilidade_parceiros' => ['json'] //
+      , 'e2_corresponde_fragilidade' => ['json']
+      , 'e2_mantem_fragilidade' => []
+      , 'e2_fragilidades_final' => ['json']
+
+
+      , 'e2_forca_fragilidade' => [] //ok
+      , 'e2_justificativa_forca_fragilidade' => ['max:900'] //ok
+      , 'e2_forca_1' => ['max:400'] //ok
+      , 'e2_forca_2' => ['max:400'] //ok
+      , 'e2_forca_3' => ['max:400'] //ok
+      , 'e2_fragilidade_1' => ['max:400'] //ok
+      , 'e2_fragilidade_2' => ['max:400'] //ok
+      , 'e2_fragilidade_3' => ['max:400'] //ok
+
+      , 'e3_como_entrei' => ['max:1200'] //ok
+      , 'e3_como_saio' => ['max:1200'] //ok
     ];
 
     public function customRules()

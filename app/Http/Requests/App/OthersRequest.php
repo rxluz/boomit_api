8e9a2dc78cff7e536925ff76ed03cccc8900d26a
rxluz <rx@appock.co>
2017-03-12 23:07:32 -0300

    mail send 3

--- app/Http/Requests/App/OthersRequest.php
+++ app/Http/Requests/App/OthersRequest.php
@@ -111,6 +111,7 @@ class OthersRequest extends ValidateRequest
             "storeContacts" => $this->storeContacts
           , "updateShellQuiz" => $this->updateShellQuiz
           , "storeShellQuiz" => $this->storeShellQuiz
+          , "storeShellQuiz1" => $this->storeShellQuiz
         ];
     }
 

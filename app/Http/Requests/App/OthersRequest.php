3845b278b5194127cde8b8649dbc4e6316851cad
rxluz <rx@appock.co>
2017-03-13 21:52:14 -0300

    others max lenght 0

--- app/Http/Requests/App/OthersRequest.php
+++ app/Http/Requests/App/OthersRequest.php
@@ -21,7 +21,7 @@ class OthersRequest extends ValidateRequest
         'e1_nome_completo' => ['required'] //ok
       , 'e1_email' => ['required', 'email'] //ok
       , 'e1_data_nascimento' => ['required'] //ok
-      , 'e1_seu_proposito' => ['max:400'] //ok
+      , 'e1_seu_proposito' => ['max:450'] //ok
       , 'e1_grau_escolaridade' => [] //ok
       , 'e1_hoje_esta' => [] //ok
       , 'e1_mora_com' => [] //ok
@@ -33,7 +33,7 @@ class OthersRequest extends ValidateRequest
       , 'e1_ideal_negocio' => [] //ok
       , 'e1_fracassou_antes' => [] //ok
       , 'e1_tempo_objetivo' => [] //ok
-      , 'e1_proposito_detalhado' => ['max:1500'] //ok
+      , 'e1_proposito_detalhado' => ['max:1550'] //ok
       , 'e2_competencias' => [] //ok
       , 'e2_fragilidades' => [] //ok
       , 'e2_melhor_forca' => [] //ok
@@ -47,15 +47,15 @@ class OthersRequest extends ValidateRequest
       , 'e2_mantem_fragilidade' => [] //ok
       , 'e2_fragilidades_final' => []
       , 'e2_forca_fragilidade' => [] //ok
-      , 'e2_justificativa_forca_fragilidade' => ['max:900'] //ok
-      , 'e2_forca_1' => ['max:400'] //ok
-      , 'e2_forca_2' => ['max:400'] //ok
-      , 'e2_forca_3' => ['max:400'] //ok
-      , 'e2_fragilidade_1' => ['max:400'] //ok
-      , 'e2_fragilidade_2' => ['max:400'] //ok
-      , 'e2_fragilidade_3' => ['max:400'] //ok1
-      , 'e3_como_entrei' => ['max:1200'] //ok
-      , 'e3_como_saio' => ['max:1200'] //ok
+      , 'e2_justificativa_forca_fragilidade' => ['max:950'] //ok
+      , 'e2_forca_1' => ['max:450'] //ok
+      , 'e2_forca_2' => ['max:450'] //ok
+      , 'e2_forca_3' => ['max:450'] //ok
+      , 'e2_fragilidade_1' => ['max:450'] //ok
+      , 'e2_fragilidade_2' => ['max:450'] //ok
+      , 'e2_fragilidade_3' => ['max:450'] //ok1
+      , 'e3_como_entrei' => ['max:1250'] //ok
+      , 'e3_como_saio' => ['max:1250'] //ok
     ];
 
 
@@ -63,7 +63,7 @@ class OthersRequest extends ValidateRequest
         'e1_nome_completo' => [] //ok
       , 'e1_email' => ['email'] //ok
       , 'e1_data_nascimento' => [] //ok
-      , 'e1_seu_proposito' => ['max:400'] //ok
+      , 'e1_seu_proposito' => ['max:450'] //ok
       , 'e1_grau_escolaridade' => [] //ok
       , 'e1_hoje_esta' => [] //ok
       , 'e1_mora_com' => [] //ok
@@ -75,7 +75,7 @@ class OthersRequest extends ValidateRequest
       , 'e1_ideal_negocio' => [] //ok
       , 'e1_fracassou_antes' => [] //ok
       , 'e1_tempo_objetivo' => [] //ok
-      , 'e1_proposito_detalhado' => ['max:1500'] //ok
+      , 'e1_proposito_detalhado' => ['max:1550'] //ok
       , 'e2_competencias' => [] //ok
       , 'e2_fragilidades' => [] //ok
       , 'e2_melhor_forca' => [] //ok
@@ -93,16 +93,16 @@ class OthersRequest extends ValidateRequest
 
 
       , 'e2_forca_fragilidade' => [] //ok
-      , 'e2_justificativa_forca_fragilidade' => ['max:900'] //ok
-      , 'e2_forca_1' => ['max:400'] //ok
-      , 'e2_forca_2' => ['max:400'] //ok
-      , 'e2_forca_3' => ['max:400'] //ok
-      , 'e2_fragilidade_1' => ['max:400'] //ok
-      , 'e2_fragilidade_2' => ['max:400'] //ok
-      , 'e2_fragilidade_3' => ['max:400'] //ok
+      , 'e2_justificativa_forca_fragilidade' => ['max:950'] //ok
+      , 'e2_forca_1' => ['max:450'] //ok
+      , 'e2_forca_2' => ['max:450'] //ok
+      , 'e2_forca_3' => ['max:450'] //ok
+      , 'e2_fragilidade_1' => ['max:450'] //ok
+      , 'e2_fragilidade_2' => ['max:450'] //ok
+      , 'e2_fragilidade_3' => ['max:450'] //ok
 
-      , 'e3_como_entrei' => ['max:1200'] //ok
-      , 'e3_como_saio' => ['max:1200'] //ok
+      , 'e3_como_entrei' => ['max:1250'] //ok
+      , 'e3_como_saio' => ['max:1250'] //ok
     ];
 
     public function customRules()

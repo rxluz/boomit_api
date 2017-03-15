28a66a41efcfb4411141fefc1f3a9b4abef9dfda
rxluz <rx@appock.co>
2017-03-14 23:37:10 -0300

    birth date 6

--- app/Models/ShellQuizHistory.php
+++ app/Models/ShellQuizHistory.php
@@ -63,7 +63,7 @@ use Illuminate\Database\Eloquent\Model;
 
   protected function getE2Forca1Attribute($value){
     $val=json_decode($value);
-    return "Força: ".$this->competencias($val->forca)."<br>Ação:".$val->acao;
+    return "Força: ".$this->competencias($val->forca)."\r\nAção:".$val->acao;
   }
 
   protected function getE2Forca2Attribute($value){

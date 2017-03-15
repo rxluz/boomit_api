eb7347581d88a3ba4ecd3cd6abfae84f833ac991
rxluz <rx@appock.co>
2017-03-14 23:29:24 -0300

    birth date

--- app/Models/ShellQuizHistory.php
+++ app/Models/ShellQuizHistory.php
@@ -61,6 +61,10 @@ use Illuminate\Database\Eloquent\Model;
 
   protected $hidden = [''];
 
+  protected function E1DataNascimentoAttribute($value){
+    return substr($value, 0, 2)."/".substr($value, 2, 2)."/".substr($value, 4, 4);
+  }
+
   protected function getE1GrauEscolaridadeAttribute($value){
     switch(strtolower($value)){
       case 'a':

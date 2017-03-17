5fae3445899fc98418b4717cf6517740e0866f8e
rxluz <rx@appock.co>
2017-03-16 22:28:24 -0300

    logo shel 3

--- resources/views/shell.blade.php
+++ resources/views/shell.blade.php
@@ -292,13 +292,29 @@ Sizes: [
 													<!-- FLEXIBLE CONTAINER // -->
 													<table border="0" cellpadding="0" cellspacing="0" width="500" class="flexibleContainer">
 														<tr>
-															<td align="center" valign="top" width="500" class="flexibleContainerCell">
+															<td align="center" valign="top" width="250" class="flexibleContainerCell">
 
 																<!-- CONTENT TABLE // -->
 																<table border="0" cellpadding="0" cellspacing="0" width="100%">
 																	<tr>
 																		<td valign="top" class="imageContent">
-																			<img src="http://pde.boomit.co/app/assets/img/logoIJ.svg" width="500" class="flexibleImage" style="max-width:500px;width:100%;display:block;" alt="Text" title="Text" />
+																			<img src="http://pde.boomit.co/app/assets/img/logoIJ.svg" width="250" class="flexibleImage" style="max-width:250px;width:100%;display:block;" alt="Shell Iniciativa Jovem" title="Shell Iniciativa Jovem" />
+																		</td>
+																	</tr>
+																</table>
+																<!-- // CONTENT TABLE -->
+
+															</td>
+														</tr>
+
+														<tr>
+															<td align="center" valign="top" width="250" class="flexibleContainerCell">
+
+																<!-- CONTENT TABLE // -->
+																<table border="0" cellpadding="0" cellspacing="0" width="100%">
+																	<tr>
+																		<td valign="top" class="imageContent">
+																			<img src="http://pde.boomit.co/app/assets/img/logoIE.svg" width="250" class="flexibleImage" style="max-width:250px;width:100%;display:block;" alt="Shell Iniciativa Empreendedora" title="Shell Iniciativa Empreendedora" />
 																		</td>
 																	</tr>
 																</table>

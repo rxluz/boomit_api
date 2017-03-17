8b946733a4037ff8e19159c429183176172d1266
rxluz <rx@appock.co>
2017-03-17 00:49:35 -0300

    report route 30

--- app/Http/Controllers/App/OthersController.php
+++ app/Http/Controllers/App/OthersController.php
@@ -320,6 +320,7 @@ class OthersController extends Controller
 
     foreach ($people as $person) {
         $person->e2_forca_1=$this->getForca($person->e2_forca_1);
+        
         $csv->insertOne($person->toArray());
     }
 

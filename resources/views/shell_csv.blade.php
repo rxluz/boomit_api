c3bbae7feea4fa0f9973e64c951a020d5664f58a
rxluz <rx@appock.co>
2017-03-16 23:59:10 -0300

    report route 11

--- resources/views/shell_csv.blade.php
+++ resources/views/shell_csv.blade.php
@@ -1,4 +1,4 @@
-e1_nome_completo,e1_email
+e1_nome_completo,e1_email <br>
 @foreach($data as $d)
-{{ $d->e1_nome_completo }},{{ $d->e1_email }}
+{{ $d->e1_nome_completo }},{{ $d->e1_email }} <br>
 @endforeach
